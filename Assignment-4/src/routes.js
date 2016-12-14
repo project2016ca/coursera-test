2cffb95001ce711dc49df78155d50ffc0236bd6c
Shweta <project2016ca@gmail.com>
2016-12-14 11:43:40 -0800

    addignment 4

---
+++ Assignment-4/src/routes.js
@@ -0,0 +1,42 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'templates/home.template.html'
+  })
+
+  // // Premade list page
+  // .state('categories', {
+  //   url: '/categories',
+  //   templateUrl: 'src/shoppinglist/templates/main-shoppinglist.template.html',
+  //   controller: 'MainShoppingListController as mainList',
+  //   resolve: {
+  //     items: ['ShoppingListService', function (ShoppingListService) {
+  //       return ShoppingListService.getItems();
+  //     }]
+  //   }
+  // })
+
+  // .state('categories.items', {
+  //   url: '/items/{itemId}',
+  //   templateUrl: 'src/shoppinglist/templates/item-detail.template.html',
+  //   controller: "ItemDetailController as itemDetail"
+  // });
+
+}
+
+})();
