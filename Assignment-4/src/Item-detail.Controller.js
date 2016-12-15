e296474acd2e84faa76688a336e7d699c3556931
Shweta <project2016ca@gmail.com>
2016-12-14 21:15:24 -0800

    addignment 4

---
+++ Assignment-4/src/Item-detail.Controller.js
@@ -0,0 +1,21 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('ItemDetailController', ItemDetailController);
+
+// 'item' is injected through state's resolve
+ItemDetailController.$inject = ['item'];
+function ItemDetailController(items) {
+  var itemDetail = this;
+  itemDetail.item = item.data;
+}
+
+})();
+
+
+
+
+
+
+
--- Assignment-4/src/menuapp.module.js
+++ Assignment-4/src/menuapp.module.js
@@ -5,24 +5,11 @@ angular.module('MenuApp', ['ui.router','data'])
 
 .controller('MainController', MainController);
 
-.controller('ItemDetailController', ItemDetailController);
-
-
-
 
 MainController.$inject = ['MenuDataService', 'items'];
 function MainController(MenuDataService, items) {
   var mainList = this;
   mainList.items = items.data;
-}
-
-
-
-
-ItemDetailController.$inject = ['MenuDataService', 'item'];
-function ItemDetailController(MenuDataService, items) {
-  var itemDetail = this;
-  itemDetail.item = item.data;
 }
 
 })();
