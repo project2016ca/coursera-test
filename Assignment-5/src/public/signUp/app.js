a46bfecca6c54326ab4471bf8aa62a44b8218dd8
Shweta <project2016ca@gmail.com>
2016-12-22 14:16:50 -0800

    addignment 5

--- Assignment-5/src/public/signUp/app.js
+++ Assignment-5/src/public/signUp/app.js
@@ -2,4 +2,16 @@
 
 angular.module('SimpleFormsApp',[]);
 
+angular.module('SimpleFormsApp')
+.controller('RegistrationController', RegistrationController);
+
+function RegistrationController() {
+  var reg = this;
+  reg.ss = "bhgbdjf";
+
+  reg.submit = function () {
+    reg.completed = true;
+  };
+}
+
 })();
