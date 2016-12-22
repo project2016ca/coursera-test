f303eb57bf03bec11748300a9c9f4b09d0888afd
Shweta <project2016ca@gmail.com>
2016-12-22 14:02:11 -0800

    addignment 5

--- Assignment-5/src/public/signUp/SignUp.controller.js
+++ Assignment-5/src/public/signUp/SignUp.controller.js
@@ -5,20 +5,18 @@ angular.module('SimpleFormsApp')
 .controller('RegistrationController', RegistrationController);
 
 
-// RegistrationController.$inject = ['saveInfoService'];
-// function RegistrationController(saveInfoService) {
-RegistrationController.$inject = [];
-function RegistrationController() {
+RegistrationController.$inject = ['saveInfoService'];
+function RegistrationController(saveInfoService) {
   var reg = this;
   reg.ss = "shweta";
-  // reg.completed = false;
-  // reg.submit = function () {
-  //   sldmsld
-  //   console.log("I am in submit function");
-  //   reg.completed = true;
-  //   saveInfoService.user = reg.user;
-  //   reg.comment = " Your Information has been saved !! ";
-  // };
+  reg.completed = false;
+  reg.submit = function () {
+    sldmsld
+    console.log("I am in submit function");
+    reg.completed = true;
+    saveInfoService.user = reg.user;
+    reg.comment = " Your Information has been saved !! ";
+  };
 }
 
 })();
